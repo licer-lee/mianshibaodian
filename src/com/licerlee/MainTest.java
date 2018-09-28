9d3ab5fd24ae647508ae32cb9297bd9e9406fa85
licerlee <licerlee@gmail.com>
2018-09-28 14:55:08 +0800

    modify MainTest

--- src/com/licerlee/MainTest.java
+++ src/com/licerlee/MainTest.java
@@ -1,18 +1,9 @@
 package com.licerlee;
 
-import com.licerlee.myprotected.ChildClass;
-import com.licerlee.myprotected.ParentClass;
-
 public class MainTest {
 
 	public static void main(String[] args) {
 	
-		ParentClass pc =new ParentClass();
-		// 不同包下类（非子类）的protected方法不能访问
-//		pc.m();
-		
-		ChildClass cc =new ChildClass();
-//		cc.m();
 	}
 	
 	
